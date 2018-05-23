0f98ad011a40c9d66084a76b6d5f24c21afbd661
Gourav Jain <gjain@xebia.com>
2018-05-23 12:56:44 +0530

    first commit to bot web app

---
+++ web-app-bot-23-05-18-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=web-app-bot-23-05-18-Web-Deploy.pubxml -p:Password=vMflLSqSnXzpcmQmznGj4Y6N9G7EnhmehHvf3jrdS3lxZhk2sb8x4fXDikpx
+
